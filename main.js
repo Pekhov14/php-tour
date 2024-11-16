4b3e0e48d0a3bde0490888f8fcc11b230d811a98
pekhov <pehovanton21@gmail.com>
2024-11-16 14:40:15 +0200

    theme

--- main.js
+++ main.js
@@ -6,58 +6,51 @@ import { createEditor } from "prism-code-editor"
 import { matchBrackets } from "prism-code-editor/match-brackets"
 import { indentGuides } from "prism-code-editor/guides"
 
-// Importing styles
-import "prism-code-editor/layout.css"
-import "prism-code-editor/scrollbar.css"
-import "prism-code-editor/themes/github-dark.css"
-
+import 'prism-code-editor/layout.css';
+import 'prism-code-editor/scrollbar.css';
+import 'prism-code-editor/themes/github-dark.css';
+import 'prism-code-editor/themes/github-light.css';
 import { loadTheme } from "prism-code-editor/themes"
 
-const isDark = matchMedia("(prefers-color-scheme: dark)").matches
+const themeToggle = document.getElementById('themeToggle');
+const lightIcon = document.getElementById('lightIcon');
+const darkIcon = document.getElementById('darkIcon');
+const editorElement = document.getElementById('editor');
 
-loadTheme(isDark ? "github-dark" : "github-light").then(theme => {
-  console.log(theme)
-})
+// let isDark = matchMedia('(prefers-color-scheme: dark)').matches;
+let isDark = false;
+
+document.body.classList.toggle("dark", isDark);
+lightIcon.classList.toggle("hidden", isDark);
+darkIcon.classList.toggle("hidden", !isDark);
+
+const loadEditorTheme = (darkMode) => {
+  loadTheme(darkMode ? 'github-dark' : 'github-light').then((theme) => {
+    console.log('Theme loaded:', theme);
+  });
+};
 
 const editor = createEditor(
   "#editor",
   {
     language: "php",
     theme: "github-dark",
+    tabSize: 4,
+    useTabs: true,
   },
   indentGuides(),
   matchBrackets(),
   () => console.log("ready"),
 )
 
-// import("./extensions")
+loadEditorTheme(isDark);
 
+themeToggle.addEventListener("click", () => {
+  isDark = !isDark;
+  document.body.classList.toggle("dark", isDark);
 
+  lightIcon.classList.toggle("hidden", isDark);
+  darkIcon.classList.toggle("hidden", !isDark);
 
-
-
-
-
-
-
-
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
+  loadTheme(isDark ? "github-dark" : "github-light");
+});
