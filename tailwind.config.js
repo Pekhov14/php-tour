73b5b2a7e253c893da733248616978422ba361df
pekhov <pehovanton21@gmail.com>
2024-12-20 03:46:05 +0200

    q

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    // "./src/**/*.{js,ts,jsx,tsx}",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      colors: {
-        primary: "#007BFF",
-        secondary: "#F9FAFB",
-        darkBg: "#1E293B",
-        darkText: "#E2E8F0",
-        contentLight: "#111827",
-        contentDark: "#CBD5E1",
-      },
+module.exports = {
+    content: [
+        "./src/**/*.{html,js}"
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
+    plugins: [],
 }
-
